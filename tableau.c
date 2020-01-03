f8ad8c0c792de91bb18feddf30898137c5185b82
Guigeek <geegeek21@gmail.com>
2020-01-03 14:30:36 +0100

    need help here

--- tableau.c
+++ tableau.c
@@ -8,9 +8,22 @@
  
 int main()
 {
-int i,j;
-int board[8][17] = {0}; //init du board avec les tokens
-unsigned t[COL][LIG];
+int i,j,j1;
+int col1=(COL-1)/2;
+int lig1=LIG-1;
+int board[col1][lig1]; //init du board avec les tokens
+char t[COL][LIG];
+
+memset(board,0,sizeof(board));
+
+for(i=0;i<col1;i++) //à modifier pour mettre le tab dans le bon sens 
+{
+	for(j=0;j<lig1;j++)
+	{
+		printf("%d",board[i][j]);
+	}
+	printf("\n");
+}
 
 for(j=0;j<COL;j++)
 	{
@@ -22,10 +35,10 @@ for(j=0;j<LIG;j=j+2)
 {
 	for(i=1;i<COL;i++)
 	{
+		j1=j/2;
 		t[i][j]='|';
-		t[i][j+1]=' '; //à remplir avec les tokens
+		t[i][j+1]=(char*) board[j1,i]; //need to fix that asap
 	}
-	
 }
 
 
