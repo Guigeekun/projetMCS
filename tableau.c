221eec1109199db36988066dfb34163c44d456de
Lucas <ndorczynski@hotmail.fr>
2020-01-09 11:03:57 +0100

    Auto stash before revert of "Tableau 'David Goodenough'"

--- tableau.c
+++ tableau.c
@@ -2,39 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 
-
- #define LIG 16
- #define COL 15
- 
+#define LIG 16
+#define COL 15
+int i;
+int j;
+char tab[COL][LIG];
 int main()
 {
-int i,j,j1;
-int col1=(COL-1)/2;
-int lig1=LIG-1;
-int board[col1][lig1]; //init du board avec les tokens
-char t[COL][LIG];
 
-memset(board,0,sizeof(board));
-
-for(i=0;i<col1;i++) //à modifier pour mettre le tab dans le bon sens 
-{
-	for(j=0;j<lig1;j++)
+	for(j=0;j<LIG;j=j+2)
 	{
-		printf("%d",board[i][j]);
-	}
-	printf("\n");
-}
 
+<<<<<<< Updated upstream
 for(j=0;j<COL;j++)
 	{
 		t[0][j]='_';
+=======
+		for(i=1;i<COL;i++)
+		{
+			tab[i][j]='|';
+			tab[i][j+1]=' ';
+		}		
+		tab[COL-1][j+1]='_';	
+>>>>>>> Stashed changes
 	}
 
-
-for(j=0;j<LIG;j=j+2)
-{
-	for(i=1;i<COL;i++)
+	for(i=0;i<LIG-1;i++) //à modifier pour mettre le tab dans le bon sens 
 	{
+<<<<<<< Updated upstream
 		j1=j/2;
 		t[i][j]='|';
 		t[i][j+1]=(char*) board[j1,i]; //need to fix that asap
@@ -61,3 +56,14 @@ for(i=0;i<COL;i++) //à modifier pour mettre le tab dans le bon sens
 
 
 
+=======
+		for(j=0;j<COL;j++)
+		{
+			printf("%c",tab[i][j]);
+		}
+		
+		printf("\n");
+		
+	}
+}
+>>>>>>> Stashed changes
